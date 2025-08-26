d72af5649702282bcdb133952bbf5950ea1b8868
hltdevlab <54474336+hltdevlab@users.noreply.github.com>
2025-08-26 23:57:20 +0800

    Delete my_config directory

--- my_config/config.py
+++
@@ -1,7 +0,0 @@
-from dynaconf import Dynaconf
-
-settings = Dynaconf(
-    envvar_prefix=False,
-    # envvar_prefix="",  # export envvars with `export DYNACONF_FOO=bar`.
-    settings_files=['my_config\somewhere\config.yaml'],  # Load files in the given order.
-)
